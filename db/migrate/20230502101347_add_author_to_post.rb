6c2723a8ae1a7991a5af8e335a6fd63f21082a4f
ram1117 <ramkumark1@outlook.com>
2023-05-02 21:10:29 +0530

    make changes to foreign key references as requested by the reviewer

--- db/migrate/20230502101347_add_author_to_post.rb
+++
@@ -1,5 +0,0 @@
-class AddAuthorToPost < ActiveRecord::Migration[7.0]
-  def change
-    add_foreign_key :posts, :users, column: :author_id, primary_key: 'id'
-  end
-end
--- db/migrate/20230502150815_create_posts.rb
+++ db/migrate/20230502150815_create_posts.rb
@@ -1,11 +1,10 @@
-class CreatePost < ActiveRecord::Migration[7.0]
+class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.integer :author_id
-      t.text :title
+      t.string :title
       t.text :text
       t.integer :comments_counter
-      t.integer :likes_counter
+      t.integer :likescounter
 
       t.timestamps
     end
