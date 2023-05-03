79812f27486db8bebcdfcbda712dfb7b041b710b
ram1117 <ramkumark1@outlook.com>
2023-05-03 23:37:13 +0530

    add test cases to User and Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,17 +4,16 @@ class Post < ApplicationRecord
   has_many :comments
   after_save :update_posts_counter
 
-  validates :title, presence: true
-  validates :title, length: { maximum: 250 }
+  validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter,
             numericality: {
               only_integer: true,
-              greater_than_or_equal_to: 0,
+              greater_than_or_equal_to: 0
             }
   validates :likes_counter,
             numericality: {
               only_integer: true,
-              greater_than_or_equal_to: 0,
+              greater_than_or_equal_to: 0
             }
 
   def update_posts_counter
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  before :all do
+    @user =
+      User.create(
+        name: 'mark',
+        photo: 'some_link',
+        bio: 'developer',
+        posts_counter: 0,
+      )
+  end
+  subject do
+    Post.new(
+      author: @user,
+      title: 'Blog 1',
+      text: 'My first tech blog',
+      comments_counter: 0,
+      likes_counter: 0,
+    )
+  end
+  describe 'check post validity' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+    it 'is invalid with title missing' do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+    it 'is invalid with title >250 chars' do
+      titletext = ''
+      251.times { titletext += 'a' }
+      subject.title = titletext
+      expect(subject).to_not be_valid
+    end
+
+    it 'is invalid with comments_counter < 0' do
+      subject.comments_counter = -5
+      expect(subject).to_not be_valid
+    end
+    it 'is invalid with comments_counter not integer' do
+      subject.comments_counter = 'five'
+      expect(subject).to_not be_valid
+    end
+
+    it 'is invalid with likes_counter < 0' do
+      subject.likes_counter = -6
+      expect(subject).to_not be_valid
+    end
+    it 'is invalid with likes_counter not integer' do
+      subject.likes_counter = 'six'
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe 'check model methods' do
+    it '#update_posts_counter' do
+      subject.update_posts_counter
+      expect(@user.posts_counter).to eql 1
+    end
+    it '#last_five_comments' do
+      6.times do |i|
+        Comment.create(author: @user, post: subject, text: "comment #{i}")
+      end
+      expect(subject.last_five_comments.length).to eql 5
+    end
+  end
+end
