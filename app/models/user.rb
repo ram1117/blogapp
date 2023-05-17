dbb83975249a0af40b7bc6fd32532e6ab46954a9
ram1117 <ramkumark1@outlook.com>
2023-05-17 13:58:51 +0530

    fix rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   validates :posts_counter,
             numericality: {
               only_integer: true,
-              greater_than_or_equal_to: 0,
+              greater_than_or_equal_to: 0
             }
 
   def last_three_posts
